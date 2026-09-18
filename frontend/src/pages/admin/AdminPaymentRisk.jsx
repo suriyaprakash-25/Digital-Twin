3d47bffe8a984902d1ad3644e880c1233752480c
SURIYA PRAKASH R M <suriyaprakashrm25@gmail.com>
2026-09-19 00:13:01 +0530

    polish: import toast hook from context core

--- frontend/src/pages/admin/AdminPaymentRisk.jsx
+++ frontend/src/pages/admin/AdminPaymentRisk.jsx
@@ -16,7 +16,7 @@ import {
   AlertCircle
 } from 'lucide-react';
 import { API_BASE_URL, getAuthHeaders } from '../../utils/api';
-import { useToast } from '../../context/ToastContext';
+import { useToast } from '../../context/toastContextCore';
 import RiskDetailsModal from '../../components/risk/RiskDetailsModal';
 
 export default function AdminPaymentRisk() {
