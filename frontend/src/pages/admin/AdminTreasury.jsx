42ef7e5e7d6d3fb7c49f555c66ffd41d5697d5d6
SURIYA PRAKASH R M <suriyaprakashrm25@gmail.com>
2026-09-19 00:12:58 +0530

    polish: import toast hook from context core

--- frontend/src/pages/admin/AdminTreasury.jsx
+++ frontend/src/pages/admin/AdminTreasury.jsx
@@ -16,7 +16,7 @@ import {
   ChevronRight
 } from 'lucide-react';
 import { API_BASE_URL, getAuthHeaders } from '../../utils/api';
-import { useToast } from '../../context/ToastContext';
+import { useToast } from '../../context/toastContextCore';
 
 export default function AdminTreasury() {
   const { showError } = useToast();
