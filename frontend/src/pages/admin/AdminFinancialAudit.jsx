fd2b07a695139201ca6cea8b25b948a372b04c1b
SURIYA PRAKASH R M <suriyaprakashrm25@gmail.com>
2026-09-19 00:13:09 +0530

    polish: import toast hook from context core

--- frontend/src/pages/admin/AdminFinancialAudit.jsx
+++ frontend/src/pages/admin/AdminFinancialAudit.jsx
@@ -16,7 +16,7 @@ import {
   Receipt
 } from 'lucide-react';
 import { API_BASE_URL, getAuthHeaders } from '../../utils/api';
-import { useToast } from '../../context/ToastContext';
+import { useToast } from '../../context/toastContextCore';
 
 export default function AdminFinancialAudit() {
   const { showError } = useToast();
